c0b31828f00bc1d90dd4fa36e0f5077ead22a0eb
Sabina Socoli <socoly.s@gmail.com>
2024-09-30 20:18:38 +0100

    Hiding API key

--- main.py
+++ main.py
@@ -1,65 +1,54 @@
-from flask import Flask, request, render_template
 import requests
+import os  # To access environment variables
+
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+# Access the API credentials securely from environment variables
+APP_ID = os.getenv('APP_ID')
+APP_KEY = os.getenv('APP_KEY')
 
-# Route for the homepage
+
 @app.route('/')
 def home():
     return render_template('home.html')
 
 
-# Route for dinner plans and recipe suggestion
-@app.route('/dinner', methods=['POST'])
-def dinner():
-    # Get dinner plans input from the user
-    dinner_plans = request.form.get('dinner_plans')
+@app.route('/submit', methods=['POST'])
+def submit():
+    ingredient = request.form['ingredient']
 
-    if dinner_plans.lower() == 'y':
-        ingredient = request.form.get('ingredient')
+    # Edamam API call with the user-provided ingredient
+    url = 'https://api.edamam.com/search'
+    params = {
+        'q': ingredient,
+        'app_id': APP_ID,  # Use the environment variable
+        'app_key': APP_KEY,  # Use the environment variable
+        'from': 0,
+        'to': 5
+    }
 
-        # Set up API credentials
-        APP_ID = 'dde6daa4'
-        APP_KEY = '009a9a1781b7e3e669a08caeb948525f'
+    response = requests.get(url, params=params)
+    data = response.json()
 
-        # Define the URL and parameters for the Edamam Recipe API
-        url = 'https://api.edamam.com/search'
-        params = {
-            'q': ingredient,
-            'app_id': APP_ID,
-            'app_key': APP_KEY,
-            'from': 0,
-            'to': 5  # Get up to 5 recipes
-        }
+    # Check if any recipes are returned
+    recipes = []
+    if 'hits' in data and len(data['hits']) > 0:
+        for hit in data['hits']:
+            recipe = hit['recipe']
+            is_vegan = 'Vegan' in recipe['healthLabels']  # Check for vegan label
+            recipes.append({
+                "name": recipe['label'],
+                "image": recipe['image'],  # Extract the image link
+                "ingredients": recipe['ingredientLines'],
+                "carbs_per_serving": round(recipe['totalNutrients']['CHOCDF']['quantity'] / recipe['yield'], 2),
+                "vegan": is_vegan,
+                "url": recipe['url']
+            })
 
-        # Make a request to the API
-        response = requests.get(url, params=params)
-        data = response.json()
-
-        # Check if recipes were returned
-        recipes = []
-        if 'hits' in data and len(data['hits']) > 0:
-            for hit in data['hits']:
-                recipe = hit['recipe']
-                is_vegan = 'Vegan' in recipe['healthLabels']
-
-                recipes.append({
-                    "name": recipe['label'],
-                    "ingredients": recipe['ingredientLines'],
-                    "carbs_per_serving": round(recipe['totalNutrients']['CHOCDF']['quantity'] / recipe['yield'], 2),
-                    "vegan": is_vegan,
-                    "url": recipe['url'],
-                    "image": recipe['image']  # Include the recipe image URL
-                })
-        else:
-            recipes = None  # No recipes found
-
-        # Pass recipes and a message back to the template
-        return render_template('home.html', message="Here are some recipe suggestions!", recipes=recipes)
-    else:
-        # If not eating in
-        return render_template('home.html', message="Enjoy your meal out!")
+    # Pass the recipes to the HTML template for display
+    return render_template('home.html', recipes=recipes)
 
 
 if __name__ == '__main__':
